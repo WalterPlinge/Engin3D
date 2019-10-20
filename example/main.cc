8b43b37165ff6bd714d1e5f26915405ef7c2793f
WalterPlinge <22519813+WalterPlinge@users.noreply.github.com>
2019-10-20 03:21:01 +0100

    Example no longer includes unnecessary headers

--- example/main.cc
+++ example/main.cc
@@ -1,13 +1,5 @@
 #include <iostream>
 
-#include <GL/glew.h>
-
-#include <GLFW/glfw3.h>
-
-#include <glm/glm.hpp>
-#define GLM_ENABLE_EXPERIMENTAL
-#include <glm/ext.hpp>
-
 #include <e3d/ogl/app.hh>
 
 
